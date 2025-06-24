64365a5821d7c250c67e71cd381aa1d0905df4e7
ntduog11 <24107946@st.phenikaa-uni.edu.vn>
2025-06-24 10:48:51 +0000

    dng

--- Nhatro/test/testkhachhang.java
+++ Nhatro/test/testkhachhang.java
@@ -2,7 +2,7 @@ public class testkhachhang {
 
     public void test(){
 
-    khachhang kh = new khachhang(name:"Nguyen Van A", tuoi:21, sdt: "0987654321", dc:"Ha Noi", gt:"nam", cmnd:"1234567890");
+    khachhang kh = new khachhang("Nguyen Van A", 21, "0987654321", "Ha Noi", "nam", "1234567890");
     kh.hienthi();
     }
 }

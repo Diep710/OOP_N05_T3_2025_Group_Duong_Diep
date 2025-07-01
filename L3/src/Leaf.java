43e2ccc2b210d23ecf999a31d7fef3bd2f95bc38
ntduog11 <24107946@st.phenikaa-uni.edu.vn>
2025-07-01 11:08:28 +0000

    dng

--- L3/src/Leaf.java
+++ L3/src/Leaf.java
@@ -4,7 +4,7 @@ public class Leaf {
  i++;
  return this; //Leaf
  }
- void print() { System.out.println(“i = ” + i); }
+ void print() { System.out.println("i = " + i); }
  public static void main(String[] args) {
  Leaf x = new Leaf();
  x.increment().increment().increment().print(); //3

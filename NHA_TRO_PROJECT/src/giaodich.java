0ea962d7652d94619d787b99aa6b9c48d0ff422b
ntduog11 <24107946@st.phenikaa-uni.edu.vn>
2025-07-09 08:03:31 +0000

    DNG

--- NHA_TRO_PROJECT/src/giaodich.java
+++ NHA_TRO_PROJECT/src/giaodich.java
@@ -1,14 +1,19 @@
 public class giaodich {
+    String hovaten;
+    String maphong;
     String ngaygiaodich;
     String thoihan;
     String tongtien;
-    public giaodich(String ngd, String th, String tt){
-
+    public giaodich(String name, String mp, String ngd, String th, String tt){
+        this.hovaten = name;
+        this.maphong = mp;
         this.ngaygiaodich = ngd;
         this.thoihan = th;
         this.tongtien = tt;
     }
     public void hienthi2(){
+        System.out.println("Ho va ten:" + hovaten);
+        System.out.println("Ma phong:" + maphong);
         System.out.println("Ngay giao dich:" + ngaygiaodich);
         System.out.println("Thoi han:" + thoihan);
         System.out.println("Tong tien:" + tongtien);
--- NHA_TRO_PROJECT/test/testgiaodich.java
+++ NHA_TRO_PROJECT/test/testgiaodich.java
@@ -1,6 +1,6 @@
 public class testgiaodich {
     public static void test2(){
-        giaodich gd = new giaodich("24/06/2025", "3 thang", "6.000.000" );
+        giaodich gd = new giaodich("Nguyen Van A", "135678", "24/06/2025", "3 thang", "6.000.000" );
         gd.hienthi2();
     }
 

95498df296cfd3cdeb7ba6171d04172e388348ae
ntduog11 <24107946@st.phenikaa-uni.edu.vn>
2025-07-12 10:57:55 +0000

    dng

--- NHA_TRO_PROJECT/src/phongtro.java
+++ NHA_TRO_PROJECT/src/phongtro.java
@@ -18,6 +18,33 @@ public class phongtro {
         System.out.println("dien tich:" + dientich);
         System.out.println("noi that:" + noithat);
     }
-
+public String setmaphong(String mp){
+    maphong = mp;
+        return maphong;
+    }
+    public void getmaphong(){
+        System.out.println("Ma phong: " + maphong);
+    }
+    public String setdiachicuthe(String dcct){
+    diachicuthe = dcct;
+        return diachicuthe;
+    }
+    public void getdiachicuthe(){
+        System.out.println("Dia chi: " + diachicuthe);
+    }
+    public String setdientich(String dt){
+        dientich = dt;
+        return dientich;
+    }
+    public void getdientich(){
+        System.out.println("Dien tich: " + dientich);
+    }
+    public String setnoithat(String nt){
+        noithat = nt;
+        return noithat;
+    }
+    public void getnoithat(){
+        System.out.println("Noi that: " + noithat);
+    }
 
 }

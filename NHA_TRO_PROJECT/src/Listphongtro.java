3cb4b36e6d1eefbc53c81a61ba23566e1127af7a
ntduog11 <24107946@st.phenikaa-uni.edu.vn>
2025-07-15 11:09:24 +0000

    dng

---
+++ NHA_TRO_PROJECT/src/Listphongtro.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Listphongtro {
+    public List<phongtro> ptro;
+    public Listphongtro(List<phongtro> pt) {
+        this.ptro = pt;
+    }
+    public List<phongtro> getList() {
+        return ptro;
+    }
+    public List<phongtro> Create(phongtro pt) {
+        ptro.add(pt);
+        return ptro;
+    }
+
+    public List<phongtro> Delete(String mp) {
+        for (int i = 0; i < ptro.size(); i++) {
+            if (ptro.get(i).maphong.equals(mp)) {
+                ptro.remove(i);
+            }
+        }
+        return ptro;
+    }
+    public List<phongtro> Edit(String mp) {
+
+        for (int i = 0; i < ptro.size(); i++) {
+            if (ptro.get(i).maphong.equals(mp)) {
+                // to do
+                System.out.println("Nhap ma phong muon sua doi: ");
+                Scanner scanName = new Scanner(System.in);
+                String newName = scanName.nextLine();
+                ptro.get(i).noithat = newName;
+
+            }
+        }
+
+        return ptro;
+    }
+
+    public void printphongtro() {
+        for (int i = 0; i < ptro.size(); i++) {
+
+            System.out.println("Ma phong tro :" + ptro.get(i).maphong + " " + ptro.get(i).noithat);
+
+        }
+    }
+
+}
---
+++ NHA_TRO_PROJECT/test/testListphongtro.java
@@ -0,0 +1,6 @@
+public class testListphongtro {
+    public static void test(){
+        Book obj1 = new Book("1", "test1");
+        Book obj2 = new Book("2", "test2");
+
+}

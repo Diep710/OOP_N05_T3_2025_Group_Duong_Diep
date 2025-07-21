a16d1d208d967da0ae2e2c1d5fdcfe3e1fecae19
Diep710 <24100250@st.phenikaa-uni.edu.vn>
2025-07-21 03:19:04 +0000

    diep

---
+++ NHA_TRO_PROJECT/review/DisruptLecture.java
@@ -0,0 +1,7 @@
+public class DisruptLecture {
+public static void main(String[] args) {
+ cellPhone noiseMaker = new cellPhone();
+ ObnoxiousTune ot = new ObnoxiousTune();
+ noiseMaker.ring(ot);
+}
+}

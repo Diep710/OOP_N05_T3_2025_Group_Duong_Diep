faa621c8c6f9f83f4e4130e40bedfec4a3c2ed30
Diep710 <24100250@st.phenikaa-uni.edu.vn>
2025-08-05 09:32:44 +0000

    diep

--- Quan li phong tro Duong-Diep/complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ Quan li phong tro Duong-Diep/complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,5 +1,7 @@
 package com.example.servingwebcontent;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,5 +15,16 @@ public class GreetingController {
 		model.addAttribute("name", name);
 		return "greeting";
 	}
-
 }
+public class KhachhangController {
+
+	@GetMapping("/Khachhang")
+	public String KhachhangController(Model model){
+		ArrayList<Khachhang> Khachhang = new ArrayList <Khachhang>();
+		Khachhang.add(new Khachhang("1","Nguyen Van A","20","0912345678","Ha Noi","Nam","00123456789"));
+		Khachhang.add(new Khachhang("2","Nguyen Thi B","19","0987654321","Ha Noi","Nu","00133456789"));
+		model.addAttribute("KH",Khachhang);
+		return "Khachhang";
+	}
+}
+	

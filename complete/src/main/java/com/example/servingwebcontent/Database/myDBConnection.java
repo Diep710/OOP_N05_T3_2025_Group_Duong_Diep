58245a4eb37bbec7a80c229e1f76c4babe52b847
ntduog11 <24107946@st.phenikaa-uni.edu.vn>
2025-08-11 07:38:59 +0000

    dng

--- complete/src/main/java/com/example/servingwebcontent/Database/myDBConnection.java
+++ complete/src/main/java/com/example/servingwebcontent/Database/myDBConnection.java
@@ -1,5 +1,6 @@
 package com.example.servingwebcontent.Database;
-
+import java.sql.SQLException;
+import com.mysql.cj.jdbc.Driver;
 import org.springframework.stereotype.Controller;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,51 +11,19 @@ import org.springframework.beans.factory.annotation.Value;
 @Controller
 public class myDBConnection {
 
-    public myDBConnection() {
-    };
+    public static Connection getConnection(){
+        Connection c = null ;
+        try{
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            String url = "jdbc:mysql://avnadmin:AVNS_DbvHWaBUrlYODvqhfWn@mysql-1acba087-ntduog11.b.aivencloud.com:17489/defaultdb?ssl-mode=REQUIRED:17489/defaultdb?ssl-mode=REQUIRED";
+            String username = "avnadmin";
+            String password = "";
+        } catch (SQLException e) {
+             e.printStackTrace();
 
-    // @Value("${my.database.url}")
-    // protected String myDatabaseURL;
-
-    String myDatabaseURL = "jdbc:mysql://avnadmin:AVNS_DbvHWaBUrlYODvqhfWn@mysql-1acba087-ntduog11.b.aivencloud.com:17489/defaultdb?ssl-mode=REQUIRED:17489/defaultdb?ssl-mode=REQUIRED";
-
-    // @Value("${my.database.driver}")
-    // protected String myDatabaseDriver;
-
-    String myDatabaseDriver = "com.mysql.cj.jdbc.Driver";
-
-    public Connection conn = null;
-
-    public Statement getMyConn() {
-
-        try {
-
-            Class.forName(myDatabaseDriver);
-            conn = DriverManager.getConnection(myDatabaseURL);
-            Statement sta = conn.createStatement();
-            return sta;
-
-        } catch (Exception e) {
-            System.out.println("myDBConnection at 34" + e);
         }
-
-        return null;
-
     }
-
-    public Connection getOnlyConn() {
-        try {
-            Class.forName(myDatabaseDriver);
-
-            conn = DriverManager.getConnection(myDatabaseURL);
-            return conn;
-
-        } catch (Exception e) {
-            System.out.println("Database connection error: " + e);
-        }
-
-        return conn;
-
-    }
-
 }
+
+
+   
